3c7753e2d83742afed4632727c3f5107e0025ac3
Axmatova <axmatovamamura0203@gmail.com>
2024-02-06 21:49:22 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home page</h1>
+      <nav>
+        <ul>
+        <li>
+            <Link href={'/'}>
+            Home
+            </Link>
+          </li>
+          <li>
+            <Link href={'/about'}>
+            About me
+            </Link>
+          </li>
+          <li>
+            <Link href={'/projects'}>
+            My projects
+            </Link>
+          </li>
+          <li>
+            <Link href={'/contact'}>
+          Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   );
 }
